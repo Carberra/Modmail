ae9adf355f638ab87f24dab3b61d61d3849f7ffc
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-11-26 14:23:52 +0000

    Add meta plugin

---
+++ modmail/extensions/meta.py
@@ -0,0 +1,154 @@
+# Copyright (c) 2020-2021, Carberra Tutorials
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import datetime as dt
+import platform
+import time
+import typing as t
+from dataclasses import dataclass
+
+import hikari
+from lightbulb import commands, context, decorators, plugins
+from psutil import Process, virtual_memory
+from pygount import SourceAnalysis
+
+import modmail
+from modmail.utils import chron, helpers
+
+if t.TYPE_CHECKING:
+    from lightbulb.app import BotApp
+
+plugin = plugins.Plugin("Meta")
+
+
+@dataclass
+class CodeCounter:
+    code: int = 0
+    docs: int = 0
+    empty: int = 0
+
+    def count(self) -> "CodeCounter":
+        for file in modmail.ROOT_DIR.rglob("*.py"):
+            analysis = SourceAnalysis.from_file(file, "pygount", encoding="utf-8")
+            self.code += analysis.code_count
+            self.docs += analysis.documentation_count
+            self.empty += analysis.empty_count
+
+        return self
+
+
+@plugin.command
+@decorators.command("ping", "Get the average DWSP latency for the bot.")
+@decorators.implements(commands.slash.SlashCommand)
+async def cmd_ping(ctx: context.base.Context) -> None:
+    await ctx.respond(
+        f"Pong! DWSP latency: {ctx.bot.heartbeat_latency * 1_000:,.0f} ms."
+    )
+
+
+@plugin.command
+@decorators.command("about", "View information about Modmail.")
+@decorators.implements(commands.slash.SlashCommand)
+async def cmd_about(ctx: context.base.Context) -> None:
+    await ctx.respond(
+        hikari.Embed(
+            title="About Modmail",
+            description="Type `/stats` for bot runtime stats.",
+            colour=helpers.choose_colour(),
+            timestamp=dt.datetime.now().astimezone(),
+        )
+        .set_thumbnail(ctx.get_guild().get_my_member().avatar_url)
+        .set_author(name="Information")
+        .set_footer(
+            f"Requested by {ctx.member.display_name}", icon=ctx.member.avatar_url
+        )
+        .add_field("Author", f"<@{385807530913169426}>")
+        .add_field(
+            "Contributors",
+            f"View on [GitHub]({modmail.__url__}/graphs/contributors)",
+        )
+        .add_field(
+            "License",
+            '[BSD 3-Clause "New" or "Revised" License]'
+            f"({modmail.__url__}/blob/main/LICENSE)",
+        )
+    )
+
+
+@plugin.command
+@decorators.command("stats", "View runtime stats for Modmail.")
+@decorators.implements(commands.slash.SlashCommand)
+async def cmd_stats(ctx: context.base.Context) -> None:
+    with (proc := Process()).oneshot():
+        uptime = chron.short_delta(
+            dt.timedelta(seconds=time.time() - proc.create_time())
+        )
+        cpu_time = chron.short_delta(
+            dt.timedelta(seconds=(cpu := proc.cpu_times()).system + cpu.user),
+            milliseconds=True,
+        )
+        mem_total = virtual_memory().total / (1024 ** 2)
+        mem_of_total = proc.memory_percent()
+        mem_usage = mem_total * (mem_of_total / 100)
+
+    await ctx.respond(
+        hikari.Embed(
+            title="Runtime statistics for Modmail",
+            description="Type `/about` for general bot information.",
+            colour=helpers.choose_colour(),
+            timestamp=dt.datetime.now().astimezone(),
+        )
+        .set_thumbnail(ctx.get_guild().get_my_member().avatar_url)
+        .set_author(name="Information")
+        .set_footer(
+            f"Requested by {ctx.member.display_name}", icon=ctx.member.avatar_url
+        )
+        .add_field("Bot version", modmail.__version__, inline=True)
+        .add_field("Python version", platform.python_version(), inline=True)
+        .add_field("Hikari version", hikari.__version__, inline=True)
+        .add_field("Uptime", uptime, inline=True)
+        .add_field("CPU time", cpu_time, inline=True)
+        .add_field(
+            "Memory usage",
+            f"{mem_usage:,.3f}/{mem_total:,.0f} MiB ({mem_of_total:,.0f}%)",
+            inline=True,
+        )
+        .add_field("Code lines", f"{ctx.bot.d.loc.code:,}", inline=True)
+        .add_field("Docs lines", f"{ctx.bot.d.loc.docs:,}", inline=True)
+        .add_field("Blank lines", f"{ctx.bot.d.loc.empty:,}", inline=True)
+    )
+
+
+def load(bot: "BotApp") -> None:
+    if not bot.d.loc:
+        bot.d.loc = CodeCounter().count()
+    bot.add_plugin(plugin)
+
+
+def unload(bot: "BotApp") -> None:
+    bot.remove_plugin(plugin)
